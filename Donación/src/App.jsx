98ebc3e79426563cf26694d3dc09ef2ca249e397
Franco Pecci <fapeccisan@gmail.com>
2023-05-26 15:27:13 -0400

    a

--- Donación/src/App.jsx
+++ Donación/src/App.jsx
@@ -2,6 +2,7 @@ import datos from './data/datos'
 import './App.css'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function App() {
   const position = [-23.442503, -58.443832]
@@ -16,10 +17,11 @@ function App() {
       datos.map((local) => {
         return <Marker position={[local.latitud, local.longitud]}>
         <Popup>
-          <span className="omar">Local: {local.local_donacion}</span><br />
           <span className="omar">Dirección: {local.direccion}</span><br />
-          <span className="omar">Hora de apertura: {local.hora_apertura}</span><br /> 
-          <span className="omar">Hora de cierre: {local.hora_cierre}</span>
+          <span className="omar">Local: {local.local_donacion}</span><br />
+          <span className="omar">Hora de apertura: {local.hora_apertura} hs.</span><br />
+          <span className="omar">Hora de cierre: {local.hora_cierre}hs.</span><br />
+          <span className="omar">Coordenadas: </span> <Link to="https://www.google.com/maps/@-25.1941667,-25.1967416,17z?entry=ttu"><span className="omar">{local.latitud}, {local.longitud}</span></Link>
         </Popup>
       </Marker>
       })
