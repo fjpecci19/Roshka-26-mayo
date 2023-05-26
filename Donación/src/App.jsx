2f25fba5dcd1ddeb985651a743f893db25de264c
Franco Pecci <fapeccisan@gmail.com>
2023-05-26 14:18:13 -0400

    a

--- Donación/src/App.jsx
+++ Donación/src/App.jsx
@@ -1,16 +1,23 @@
-import Donacion from './componentes/Donacion'
 import datos from './data/datos'
 import './App.css'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
 
 function App() {
-  const list = datos.map((d) => {
-    return <Donacion element = {d}/>
-  })
-  
-  return (
-    <div>
-      {list}
-    </div>
+  const position = [51.505, -0.09]
+        
+  return(
+  <MapContainer className="coso" center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
   )
 }
 
