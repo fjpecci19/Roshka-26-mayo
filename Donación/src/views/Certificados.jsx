e90498767d7ee91c9a8730bec850052ee9d1f8b7
Franco Pecci <fapeccisan@gmail.com>
2023-06-05 14:00:15 -0400

    a

--- Donación/src/views/Certificados.jsx
+++ Donación/src/views/Certificados.jsx
@@ -46,19 +46,19 @@ function Certificados() {
           </div>
       </div>
       <h2 className="titulo">Certificados</h2>
+      <h2><button className="buttoon"><Link className="Linkk" to={"/NewCertificado"}>Generar nuevo certificado</Link></button></h2>
       <div className="certif">
         <h2></h2>
         {certi.map((certificado, index) => (
           <div key={index} className="title">Certificado de donación
             <br /><br /><div className="datos"><span>Establecimiento:</span><span>{certificado.local_donacion}</span></div>
-            <br /><div className="datos"><span>Apellido:</span><span>{certificado.surname}</span></div>
-            <br /><div className="datos"><span>Nombre:</span><span>{certificado.name}</span></div>
-            <br /><div className="datos"><span>Sexo:</span><span>Masculino</span></div>
-            <br /><div className="datos"><span>Cédula de identidad:</span><span>4563214</span></div>
-            <br /><div className="datos"><span>Última donación:</span><span>16/4/2021</span></div>
+            <br /><div className="datos"><span>Apellido:</span><span>{certificado.user.surname}</span></div>
+            <br /><div className="datos"><span>Nombre:</span><span>{certificado.user.name}</span></div>
+            <br /><div className="datos"><span>Sexo:</span><span>{certificado.user.sexo}</span></div>
+            <br /><div className="datos"><span>Cédula de identidad:</span><span>{certificado.user.nro_cedula}</span></div>
+            <br /><div className="datos"><span>Última donación:</span><span>{certificado.user.ult_vez_donado}</span></div>
           </div>
         ))}<br /><br />
-        <h2><button className="buttoon"><Link className="Linkk" to={"/NewCertificado"}>Generar nuevo certificado</Link></button></h2>
       </div>
     </div>
   )
